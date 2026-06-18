6bde26e20f8cdc0da4f36734db0c1da5aa897b79
prakash-TM <prakurms17@gmail.com>
2026-06-18 13:51:28 +0530

    added workflow for the app deployment in gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Creating_Docker_Images_For_React_App-vite-/",
+});
